ac03680ff96964ff524b592ca4bc4167c116e853
Punitboriya <126247444+Punitboriya@users.noreply.github.com>
2023-11-26 14:45:52 +0530

    Add files via upload

---
+++ SpringBootFile_JSON_Together/src/main/java/com/acme/SpringBootFile_JSON_Together/Entity/User.java
@@ -0,0 +1,10 @@
+package com.acme.SpringBootFile_JSON_Together.Entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private String city;
+}
